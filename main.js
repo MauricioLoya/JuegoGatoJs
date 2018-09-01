9a41c97a5be569a314e5e334c42773c0470db71a
MauricioLoya <mauloy15@hotmail.com>
2018-09-01 14:28:19 -0500

    Aplicando el ECS6

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-
-var tablero = document.getElementById('tablero');
-startGame();
-var turno = true;
-const cross = '<i class="fas fa-times"></i>';
-const circle = '<i class="far fa-circle"></i>';
-var btn = document.getElementsByClassName('btn');
-
-function startGame() {
-    var casillas = "";
-    for (var i = 1; i <= 9; i++) {
-        casillas += '<button id=' + i +
-            ' onclick="press(this)" class="btn btn-outline-info casilla"></button>';
+let startGame = () => {
+    let casillas = '';
+    for (let i = 1; i <= 9; i++) {
+        casillas += `<button id="${i}" 
+        onclick="press(this)" 
+        class="btn btn-outline-info casilla"></button>`
         if (i % 3 == 0) {
-            casillas += '<br>'
+            casillas += `<br>`
         }
     }
     tablero.innerHTML = casillas;
 }
 
-function press(boton) {
+const tablero = document.getElementById('tablero');
+startGame();
+let turno = true;
+const cross = `<i class="fas fa-times"></i>`;
+const circle = `<i class="far fa-circle"></i>`;
+let btn = document.getElementsByClassName('btn');
+
+
+
+let press = (boton) => {
     if (!boton.innerHTML.length > 0) {
         if (turno) {
             boton.innerHTML = cross;
@@ -30,111 +32,103 @@ function press(boton) {
     }
 }
 
-function restartGame() {
+let showWinner = (ganador) => {
+    tablero.innerHTML = `<h3>El jugador <strong>
+                        ${ganador}</strong>
+                        ganó</h3>`;
+}
+
+let restartGame = () => {
     turno = true;
     tablero.innerHTML = '';
     startGame();
     btn = document.getElementsByClassName('btn');
 }
 
-function findWinner() {
+let findWinner = () => {
     //Lineas Horizontales
     if (btn[0].innerHTML == circle &&
         btn[1].innerHTML == circle &&
         btn[2].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[0].innerHTML == cross &&
         btn[1].innerHTML == cross &&
         btn[2].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     if (btn[3].innerHTML == circle &&
         btn[4].innerHTML == circle &&
         btn[5].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[3].innerHTML == cross &&
         btn[4].innerHTML == cross &&
         btn[5].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     if (btn[6].innerHTML == circle &&
         btn[7].innerHTML == circle &&
         btn[8].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[6].innerHTML == cross &&
         btn[7].innerHTML == cross &&
         btn[8].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     //Lineas Verticales
     if (btn[0].innerHTML == circle &&
         btn[3].innerHTML == circle &&
         btn[6].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
+
     }
     if (btn[0].innerHTML == cross &&
         btn[3].innerHTML == cross &&
         btn[6].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
+
     }
     if (btn[1].innerHTML == circle &&
         btn[4].innerHTML == circle &&
         btn[7].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[1].innerHTML == cross &&
         btn[4].innerHTML == cross &&
         btn[7].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     if (btn[2].innerHTML == circle &&
         btn[5].innerHTML == circle &&
         btn[8].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[2].innerHTML == cross &&
         btn[5].innerHTML == cross &&
         btn[8].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     //Linas Cruzadas
     if (btn[0].innerHTML == circle &&
         btn[4].innerHTML == circle &&
         btn[8].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[0].innerHTML == cross &&
         btn[4].innerHTML == cross &&
         btn[8].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
     if (btn[6].innerHTML == circle &&
         btn[4].innerHTML == circle &&
         btn[2].innerHTML == circle) {
-        console.log('gano player 2');
-        document.playerTwo.submit();
+        showWinner('2');
     }
     if (btn[6].innerHTML == cross &&
         btn[4].innerHTML == cross &&
         btn[2].innerHTML == cross) {
-        console.log('gano player 1');
-        document.playerOne.submit();
+        showWinner('1');
     }
 }
